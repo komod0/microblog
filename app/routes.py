76c4eaee2b750a57df96916719637718c4ae4947
komod0 <juliancrespo15@gmail.com>
2019-01-03 23:00:56 -0300

    Agrego condicionales y loops

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Julito'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in tuvieja!'
+        },
+        {
+            'author': {'username': 'Susan   '},
+            'body': 'The avengers movie was so cool '
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
